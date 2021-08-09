74fc77f6982a5e3477dc2fe58138f17f51f77d11
Aleksandra1311 <77723241+Aleksandra1311@users.noreply.github.com>
2021-08-09 12:25:12 +0300

    added
    added

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,3 @@
-// В ECMAScript modules исп другой синтаксис
-
-// import TodosController from './controller/TodosController' // но это пока не работает, в процессе внедрения
 import TodosController from '../js/controller/TodosController';
 
 $(() => {
