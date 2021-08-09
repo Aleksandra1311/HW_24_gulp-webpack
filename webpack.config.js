29a1bf73866d3f9a6cd3f03477e7070ccfc6c735
Aleksandra1311 <77723241+Aleksandra1311@users.noreply.github.com>
2021-08-09 12:22:46 +0300

    added
    added

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/app.js', // точка входа, начальный файл
+    entry: './src/js/app.js',
     output: {
-        filename: 'bundle.js', // то куда положить файл
-        path: path.resolve(__dirname, 'dist'), // путь куда положить
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'), 
     },
      plugins: [
         new HtmlWebpackPlugin({
@@ -13,11 +13,11 @@ module.exports = {
             template: './src/index.html',
         }),
     ],
-    module: { // сюда подключаются разные loaders
-        rules: [ // правила по которым подключ loader
+    module: { 
+        rules: [ 
             {
-                test: /\.css$/, //это определённая маска для подкл css файл заканчивается на css
-                use: ['style-loader', 'css-loader'], //писать в обратном порядке (css-loader умеет читать css файлы)(style-loader он умеет записывать их инлайново)
+                test: /\.css$/, 
+                use: ['style-loader', 'css-loader'], 
             }
         ],
     }
